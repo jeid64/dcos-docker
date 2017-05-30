6eed7a22020f9fe56b3f1508ac84a5d70c64e4f5
Julien Eid <jeid@mesosphere.com>
2017-05-30 18:45:16 -0400

    Added num_masters+label

--- me/script.sh
+++ me/script.sh
@@ -4,13 +4,14 @@ mkdir genconf/serve
 mkdir genconf/tmp
 dcos_dad_bootstrap_ip=10.0.5.65
 dcos_dad_bootstrap_port=8002
-cat dcos-dad-master.json | sed -e "s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/" >| genconf/tmp/dcos-dad-master.json
+dcos_dad_num_masters=3
+cat dcos-dad-master.json | sed -e "s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/;s/dcos_dad_num_masters/$dcos_dad_num_masters/" >| genconf/tmp/dcos-dad-master.json
 cat bootstrap_serve/dcos-dad-envfile-master | sed -e "s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/" >| genconf/serve/dcos-dad-envfile-master
 cat dcos-dad-slave.json | sed -e "s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/" >| genconf/tmp/dcos-dad-slave.json
 cat bootstrap_serve/dcos-dad-envfile-slave | sed -e "s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/" >| genconf/serve/dcos-dad-envfile-slave
 dcos_dad_cryptoid=4s94a3xtetxwedpkfoqrulwn399mud6exteg3tmj8iai73tzsruy
 dcos_dad_upstream_resolver=10.0.5.65
-cat bootstrap_serve/config.yaml | sed -e "s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/;s/dcos_dad_cryptoid/$dcos_dad_cryptoid/;s/dcos_dad_upstream_resolver/$dcos_dad_upstream_resolver/" >| genconf/config.yaml
+cat bootstrap_serve/config.yaml | sed -e "s/dcos_dad_num_masters/$dcos_dad_num_masters/;s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/;s/dcos_dad_cryptoid/$dcos_dad_cryptoid/;s/dcos_dad_upstream_resolver/$dcos_dad_upstream_resolver/" >| genconf/config.yaml
 cp bootstrap_serve/ip-detect genconf/ip-detect
 
 bash dcos_generate_config.ee.sh
