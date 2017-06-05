541b74c913e8641da51af88a11aed345a20a2bb7
Julien Eid <jeid@mesosphere.com>
2017-06-05 19:00:05 -0400

    Added instructions to README.

--- me/script.sh
+++ me/script.sh
@@ -1,17 +1,17 @@
 set -o noclobber
+dcos_dad_bootstrap_ip=10.0.5.65
+dcos_dad_bootstrap_port=8002
+dcos_dad_upstream_resolver=10.0.5.65
+dcos_dad_num_masters=3
+dcos_dad_vhost=jeid-6hu0-publicsl-cipbg0zxm7eg-591045150.us-west-2.elb.amazonaws.com
+dcos_dad_cryptoid=4s94a3xtetxwedpkfoqrulwn399mud6exteg3tmj8iai73tzsruy
 mkdir genconf
 mkdir genconf/serve
 mkdir genconf/tmp
-dcos_dad_bootstrap_ip=10.0.5.65
-dcos_dad_bootstrap_port=8002
-dcos_dad_num_masters=3
-dcos_dad_vhost=jeid-6hu0-publicsl-cipbg0zxm7eg-591045150.us-west-2.elb.amazonaws.com
 cat dcos-dad-master.json | sed -e "s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/;s/dcos_dad_num_masters/$dcos_dad_num_masters/;s/dcos_dad_vhost/$dcos_dad_vhost/" >| genconf/tmp/dcos-dad-master.json
 cat bootstrap_serve/dcos-dad-envfile-master | sed -e "s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/" >| genconf/serve/dcos-dad-envfile-master
 cat dcos-dad-slave.json | sed -e "s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/" >| genconf/tmp/dcos-dad-slave.json
 cat bootstrap_serve/dcos-dad-envfile-slave | sed -e "s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/" >| genconf/serve/dcos-dad-envfile-slave
-dcos_dad_cryptoid=4s94a3xtetxwedpkfoqrulwn399mud6exteg3tmj8iai73tzsruy
-dcos_dad_upstream_resolver=10.0.5.65
 cat bootstrap_serve/config.yaml | sed -e "s/dcos_dad_num_masters/$dcos_dad_num_masters/;s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/;s/dcos_dad_cryptoid/$dcos_dad_cryptoid/;s/dcos_dad_upstream_resolver/$dcos_dad_upstream_resolver/" >| genconf/config.yaml
 cp bootstrap_serve/ip-detect genconf/ip-detect
 
@@ -20,3 +20,4 @@ docker run -d -p $dcos_dad_bootstrap_port:80 -v $PWD/genconf/serve:/usr/share/ng
 
 curl -X POST http://localhost:8080/v2/apps -d @genconf/tmp/dcos-dad-master.json -H "Content-type: application/json"
 curl -X POST http://localhost:8080/v2/apps -d @genconf/tmp/dcos-dad-slave.json -H "Content-type: application/json"
+curl -X POST http://localhost:8080/v2/apps -d @haproxy-master/haproxy-masters.json -H "Content-type: application/json"
