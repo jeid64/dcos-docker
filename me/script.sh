04b81458a5f406189712b09042cf2a4bbde3663e
Julien Eid <jeid@mesosphere.com>
2017-06-05 17:38:00 -0400

    Added vhost+mlb options for master.

--- me/script.sh
+++ me/script.sh
@@ -5,7 +5,8 @@ mkdir genconf/tmp
 dcos_dad_bootstrap_ip=10.0.5.65
 dcos_dad_bootstrap_port=8002
 dcos_dad_num_masters=3
-cat dcos-dad-master.json | sed -e "s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/;s/dcos_dad_num_masters/$dcos_dad_num_masters/" >| genconf/tmp/dcos-dad-master.json
+dcos_dad_vhost=jeid-6hu0-publicsl-cipbg0zxm7eg-591045150.us-west-2.elb.amazonaws.com
+cat dcos-dad-master.json | sed -e "s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/;s/dcos_dad_num_masters/$dcos_dad_num_masters/;s/dcos_dad_vhost/$dcos_dad_vhost/" >| genconf/tmp/dcos-dad-master.json
 cat bootstrap_serve/dcos-dad-envfile-master | sed -e "s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/" >| genconf/serve/dcos-dad-envfile-master
 cat dcos-dad-slave.json | sed -e "s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/" >| genconf/tmp/dcos-dad-slave.json
 cat bootstrap_serve/dcos-dad-envfile-slave | sed -e "s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/" >| genconf/serve/dcos-dad-envfile-slave
