f242f905e063e5d10e64742de8a42741a462b119
Julien Eid <jeid@mesosphere.com>
2017-06-06 10:14:38 -0400

    Change to fetch SSH key.

--- dcos-dad/include/dcos-dad-download.sh
+++ dcos-dad/include/dcos-dad-download.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#SSH key fetch and place.
+mkdir -p /root/.ssh
+cp /mnt/mesos/sandbox/authorized_keys /root/.ssh/
+
+# Now let's install DC/OS.
 mkdir /tmp/dcos && cd /tmp/dcos
 curl -O ${dcos_bootstrap_private_ip}:${dcos_bootstrap_port}/dcos_install.sh
 /bin/bash dcos_install.sh --no-block-dcos-setup ${dcos_role}
