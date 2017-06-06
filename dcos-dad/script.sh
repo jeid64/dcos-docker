67c2e6a32a5d6c947b4a990dd0ccefae3b440181
Julien Eid <jeid@mesosphere.com>
2017-06-06 16:24:51 -0400

    Added cluster prefix.

--- dcos-dad/script.sh
+++ dcos-dad/script.sh
@@ -5,14 +5,15 @@ set -o noclobber
 : ${dcos_dad_num_masters:=3}
 : ${dcos_dad_vhost:=jeid-6hu0-publicsl-cipbg0zxm7eg-591045150.us-west-2.elb.amazonaws.com}
 : ${dcos_dad_cryptoid:=4s94a3xtetxwedpkfoqrulwn399mud6exteg3tmj8iai73tzsruy}
+: ${dcos_dad_prefix:=jeid}
 mkdir genconf
 mkdir genconf/serve
 mkdir genconf/tmp
-cat dcos-dad-master.json | sed -e "s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/;s/dcos_dad_num_masters/$dcos_dad_num_masters/;s/dcos_dad_vhost/$dcos_dad_vhost/" >| genconf/tmp/dcos-dad-master.json
+cat dcos-dad-master.json | sed -e "s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/;s/dcos_dad_num_masters/$dcos_dad_num_masters/;s/dcos_dad_vhost/$dcos_dad_vhost/;s/dcos_dad_prefix/$dcos_dad_prefix/" >| genconf/tmp/dcos-dad-master.json
 cat bootstrap_serve/dcos-dad-envfile-master | sed -e "s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/" >| genconf/serve/dcos-dad-envfile-master
-cat dcos-dad-slave.json | sed -e "s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/" >| genconf/tmp/dcos-dad-slave.json
+cat dcos-dad-slave.json | sed -e "s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/;s/dcos_dad_prefix/$dcos_dad_prefix/" >| genconf/tmp/dcos-dad-slave.json
 cat bootstrap_serve/dcos-dad-envfile-slave | sed -e "s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/" >| genconf/serve/dcos-dad-envfile-slave
-cat bootstrap_serve/config.yaml | sed -e "s/dcos_dad_num_masters/$dcos_dad_num_masters/;s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/;s/dcos_dad_cryptoid/$dcos_dad_cryptoid/;s/dcos_dad_upstream_resolver/$dcos_dad_upstream_resolver/" >| genconf/config.yaml
+cat bootstrap_serve/config.yaml | sed -e "s/dcos_dad_num_masters/$dcos_dad_num_masters/;s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/;s/dcos_dad_cryptoid/$dcos_dad_cryptoid/;s/dcos_dad_upstream_resolver/$dcos_dad_upstream_resolver/;s/dcos_dad_prefix/$dcos_dad_prefix/" >| genconf/config.yaml
 cp bootstrap_serve/ip-detect genconf/ip-detect
 
 sh dcos_generate_config.ee.sh
