62a60e4b88a5c1836599dcd0055bf871bc994e6b
Julien Eid <jeid@mesosphere.com>
2017-06-07 08:09:45 -0400

    Started adding custom slave cpu+mem.

--- dcos-dad/script.sh
+++ dcos-dad/script.sh
@@ -6,13 +6,15 @@ set -o noclobber
 : ${dcos_dad_vhost:=jeid-6hu0-publicsl-cipbg0zxm7eg-591045150.us-west-2.elb.amazonaws.com}
 : ${dcos_dad_cryptoid:=4s94a3xtetxwedpkfoqrulwn399mud6exteg3tmj8iai73tzsruy}
 : ${dcos_dad_prefix:=jeid}
+: ${dcos_dad_slave_cpu:=2}
+: ${dcos_dad_slave_mem:=2500}
 mkdir genconf
 mkdir genconf/serve
 mkdir genconf/tmp
 cat dcos-dad-master.json | sed -e "s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/;s/dcos_dad_num_masters/$dcos_dad_num_masters/;s/dcos_dad_vhost/$dcos_dad_vhost/;s/dcos_dad_prefix/$dcos_dad_prefix/" >| genconf/tmp/dcos-dad-master.json
 cat bootstrap_serve/dcos-dad-envfile-master | sed -e "s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/" >| genconf/serve/dcos-dad-envfile-master
-cat dcos-dad-slave.json | sed -e "s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/;s/dcos_dad_prefix/$dcos_dad_prefix/" >| genconf/tmp/dcos-dad-slave.json
-cat bootstrap_serve/dcos-dad-envfile-slave | sed -e "s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/" >| genconf/serve/dcos-dad-envfile-slave
+cat dcos-dad-slave.json | sed -e "s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/;s/dcos_dad_prefix/$dcos_dad_prefix/;s/dcos_dad_slave_cpu/$dcos_dad_slave_cpu/;s/dcos_dad_slave_mem/$dcos_dad_slave_mem/" >| genconf/tmp/dcos-dad-slave.json
+cat bootstrap_serve/dcos-dad-envfile-slave | sed -e "s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/;s/dcos_dad_slave_cpu/$dcos_dad_slave_cpu/;s/dcos_dad_slave_mem/$dcos_dad_slave_mem/" >| genconf/serve/dcos-dad-envfile-slave
 cat bootstrap_serve/config.yaml | sed -e "s/dcos_dad_num_masters/$dcos_dad_num_masters/;s/dcos_dad_bootstrap_ip/$dcos_dad_bootstrap_ip/;s/dcos_dad_bootstrap_port/$dcos_dad_bootstrap_port/;s/dcos_dad_cryptoid/$dcos_dad_cryptoid/;s/dcos_dad_upstream_resolver/$dcos_dad_upstream_resolver/;s/dcos_dad_prefix/$dcos_dad_prefix/" >| genconf/config.yaml
 cp bootstrap_serve/ip-detect genconf/ip-detect
 
