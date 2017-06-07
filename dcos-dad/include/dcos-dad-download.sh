266465de07f065062acaeb09d7dc208425eaca0a
Julien Eid <jeid@mesosphere.com>
2017-06-07 08:17:37 -0400

    Forgot a then in the if statement.

--- dcos-dad/include/dcos-dad-download.sh
+++ dcos-dad/include/dcos-dad-download.sh
@@ -5,6 +5,7 @@ cp /mnt/mesos/sandbox/authorized_keys /root/.ssh/
 
 # Setup a mesos resources file if we're an agent/pub agent.
 if [ "$dcos_role" = "slave" ] || [ "$dcos_role" = "slave_public" ]
+then
     echo MESOS_RESOURCES=\'$MESOS_RESOURCES\' > /var/lib/dcos/mesos-slave-common
 fi
 
